2848b52ba6640d8773d7fa1b4282f84d3aec76cc
ysk-miya <mymtysk1130@icloud.com>
2019-11-22 15:31:44 +0900

    setState

--- src/App.js
+++ src/App.js
@@ -2,38 +2,54 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
 
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "Noname", age: 3 }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  )
+const App = () => (<Counter></Counter>)
+// const profiles = [
+//   { name: "Taro", age: 10 },
+//   { name: "Hanako", age: 5 },
+//   { name: "Noname", age: 3 }
+// ]
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    // console.log("handlePlusButton")
+    // console.log(this.state.count)
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+  //setRtateが実行されるとrender()が実行されるようになっている
+  render() {
+    return (
+      <React.Fragment>
+        <div>count:{this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User = (props) => {
-  return <div>Hi!,I am{props.name},and {props.age}years old</div>
-}
+
 
 // User.defaultProps = {
 //   age: 1
 // }
-User.PropTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
+// User.PropTypes = {
+//   name: PropTypes.string,
+//   age: PropTypes.number.isRequired
+// }
 
 export default App;
 
